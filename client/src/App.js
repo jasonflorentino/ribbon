5bbadcfd92cc0819143cbef8eb8776b9937b9958
Jason Florentino <jasonmflorentino@gmail.com>
2021-03-25 14:44:42 -0400

    Create List page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import List from './pages/List/List';
 import './styles/App.scss';
 
 function App()
@@ -14,6 +15,9 @@ function App()
             />
             <Route path="/login/" render={(props) => {
               return <Login {...props} />}}  
+            />
+            <Route path="/user/:userUuid/list/:listId" render={(props) => {
+              return <List {...props} />}}  
             />
           </Switch>
         </BrowserRouter>
---
+++ client/src/pages/List/List.js
@@ -0,0 +1,12 @@
+import "./List.scss";
+
+function List()
+{
+  return (
+    <main>
+      List Page
+    </main>
+  )
+}
+
+export default List;
