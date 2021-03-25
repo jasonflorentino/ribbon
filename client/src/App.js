70503e465a9e143377bf535882e685f8b6d2b850
Jason Florentino <jasonmflorentino@gmail.com>
2021-03-25 09:43:43 -0400

    Convert class state to useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import ButtonPrimary from './components/ButtonPrimary/ButtonPrimary';
 import InputType from './components/InputType/InputType';
 import HeroHeading from './components/HeroHeading/HeroHeading';
 import './styles/App.scss';
 
-class App extends React.Component
+function App()
 {
-  state = {
-    confirmPassword: ""
-  }
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleConfirmPasswordChange = e => setConfirmPassword(e.target.value);
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  render()
-  {
-    const {confirmPassword} = this.state;
-    return (
-      <div className="App">
-        <HeroHeading text="You go, gifter!" />
-        <InputType
-          type={"password"}
-          name={"confirmPassword"}
-          value={confirmPassword} 
-          placeholder={"Confirm password"} 
-          onChange={this.handleChange}  
-        />
-        <ButtonPrimary text="Sign up" />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <HeroHeading text="You go, gifter!" />
+      <InputType
+        type={"password"}
+        name={"confirmPassword"}
+        value={confirmPassword} 
+        placeholder={"Confirm password"} 
+        onChange={handleConfirmPasswordChange}  
+      />
+      <ButtonPrimary text="Sign up" />
+    </div>
+  );
 }
 
 export default App;
