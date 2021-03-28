7f5079aaa2d08619b0c0857c535a4e4781bb0bf7
Jason Florentino <jasonmflorentino@gmail.com>
2021-03-28 17:54:17 -0400

    Auth: Handle logout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,13 +4,19 @@ import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
 import Logo from "../Logo/Logo";
 import "./Header.scss";
 
-function Header({color, login, signUp, history})
+function Header({color, login, signUp, logout, history})
 {
+  const handleLogout = () => {
+    sessionStorage.removeItem("authToken");
+    window.location.reload()
+  }
+
   return (
     <header className={`Header`}>
       <Logo color={color} />
       <div className="Header__actions">
-        {login && <Link className="Header__Login" to="/login">Login</Link>}
+        {login && <Link className="Header__login" to="/login">Login</Link>}
+        {logout && <Link className="Header__logout" to="/" onClick={handleLogout}>Logout</Link>}
         {signUp && <ButtonPrimary 
           text="Sign up" 
           onClick={() => history.push("/signup")} 
