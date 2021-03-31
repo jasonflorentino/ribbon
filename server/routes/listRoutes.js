dabf5454873effcc4da7819db5bb322e81064722
Jason Florentino <jasonmflorentino@gmail.com>
2021-03-31 19:23:45 -0400

    Update queries for list and gift route

--- server/routes/listRoutes.js
+++ server/routes/listRoutes.js
@@ -4,34 +4,28 @@ const User = require("../models/user");
 const List = require("../models/list");
 const Gift = require("../models/gift");
 const GiftDetail = require("../models/gift_detail");
+const Bookshelf = require("../bookshelf");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
 
-  User
-    .where({ uuid: req.decode.user })
-    .fetch()
-    .then(user => {
-      List
-        .where({ user_id: user.id })
-        .fetch()
-        .then(list => {
-          Gift
-            .where({ list_id: list.id })
-            .fetchAll({withRelated: ["gift_detail"]})
-            .then(gifts => {
-              res.status(200).json(gifts);
-              utils.logResponse(res);
-              return;
-            })
-        })
-    })
-    .catch(err => {
-      console.log("DB Query Error:", err.message);
-      res.status(500).json({ message: "Error fetching data" });
-      utils.logResponse(res);
-    })
+  const id = req.decode.user;
+  const query = (id) => {
+    return `SELECT g.id, g.name, g.list_id, g.status, g.date_modified, g.date_created,gd.image, gd.price, gd.color, gd.size, gd.description, gd.category, gd.external_link FROM gifts AS g INNER JOIN gift_details AS gd ON gd.gift_id = g.id 
+    WHERE g.list_id IN (SELECT users.id FROM users WHERE users.uuid = '${id}');`
+  }
+
+  Bookshelf.knex.raw(query(id))
+  .then(data => {
+    res.status(200).json(data[0])
+    utils.logResponse(res); 
+  })
+  .catch(err => {
+    console.log("DB Query Error:", err.message);
+    res.status(500).json({ message: "Error fetching data" });
+    utils.logResponse(res);
+  })
 })
 
 module.exports = router;
