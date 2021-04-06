ba2cc076883bb50f040966df65e6d2b59bcb2ae5
Jason Florentino <jasonmflorentino@gmail.com>
2021-04-06 14:18:39 -0400

    [server] Create user profile endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -27,4 +27,19 @@ router.get("/:id", (req, res) => {
   })
 })
 
+router.get("/profile/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "SELECT p.first_name, p.last_name, p.date_of_birth, p.image, p.interests, p.allergies, p.sizes, p.user_id FROM people as p INNER JOIN users ON users.id = p.user_id WHERE (users.uuid = :id)"
+
+  Bookshelf.knex.raw(query, {id: id})
+    .then(result => {
+      res.status(200).json(result[0]);
+    })
+    .catch(err => {
+      console.log("ERROR in userRoutes GET /profile/:id - ", err.message);
+      res.status(500).json({message: "Couldn't fetch data"})
+      utils.logResponse(res);
+    })
+})
+
 module.exports = router;
