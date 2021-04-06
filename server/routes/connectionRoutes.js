d0cf48eb44e58520ad8f7fc929b62de3fa294e87
Jason Florentino <jasonmflorentino@gmail.com>
2021-04-06 19:36:25 -0400

    [server] Create endpoint to add new connection The first time a user navigates to another user's list check if they are already connections if not, create a new connection

--- server/routes/connectionRoutes.js
+++ server/routes/connectionRoutes.js
@@ -26,4 +26,46 @@ router.get("/", (req, res) => {
     })
 })
 
+router.get("/check/:id", (req, res) => {
+  const targetUuid = req.params.id;
+  const uuid = req.decode.user;
+  const queryConnections = "SELECT * FROM connections AS c WHERE (c.requester_id = (SELECT users.id FROM users WHERE users.uuid = :uuid)) OR (addressee_id = (SELECT users.id FROM users WHERE users.uuid = :uuid));" 
+  const queryIds = "SELECT u.id, u.uuid FROM users AS u WHERE u.uuid = :uuid OR u.uuid = :targetUuid;"
+  const queryMakeConnection = "INSERT INTO connections (requester_id, addressee_id) VALUES (:currUserId, :targetUserId)";
+
+  Promise.all([
+    Bookshelf.knex.raw(queryConnections, {uuid: uuid}),
+    Bookshelf.knex.raw(queryIds, {uuid: uuid, targetUuid: targetUuid})
+  ])
+    .then(result => {
+      const connections = result[0][0];
+      const users = result[1][0];
+      const currUser = users.filter(row => row.uuid === uuid)[0];
+      const targetUser = users.filter(row => row.uuid === targetUuid)[0];
+
+      if (connections.length === 0) {
+        return Bookshelf.knex.raw(queryMakeConnection, {currUserId: currUser.id, targetUserId: targetUser.id});
+      } else {
+        const existingConnections = connections.filter(
+          connection => connection.requester_id === targetUser.id 
+                     || connection.addressee_id === targetUser.id
+        );
+        if (existingConnections.length === 0) {
+          return Bookshelf.knex.raw(queryMakeConnection, {currUserId: currUser.id, targetUserId: targetUser.id});
+        } else {
+          return false;
+        }
+      }
+    })
+    .then(result => {
+      res.status(200).json({connectionCreated: !!result});
+      utils.logResponse(res);
+    })
+    .catch(err => {
+      console.log("ERROR: HEAD to connections/", err.message);
+      res.status(500).json({ message: "There was an error with the server" });
+      utils.logResponse(res);
+    })
+})
+
 module.exports = router;
