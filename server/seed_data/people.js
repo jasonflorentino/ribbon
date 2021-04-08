7b01fa92237a7d8af08452db5a6d103398997b0f
Jason Florentino <jasonmflorentino@gmail.com>
2021-04-08 18:22:33 -0400

    [server] Update seed data and profile images

--- server/seed_data/people.js
+++ server/seed_data/people.js
@@ -1,38 +1,38 @@
 module.exports = [
   {
-    first_name: "Nolan",
+    first_name: "Natalie",
     last_name: "Knill",
     date_of_birth: "1991-01-01",
     interests: "Back-end is life.",
     allergies: "Saying it \"My-SeeQuel\"",
     sizes: "Shirt M, Shoe 10.5, API Surface: Large",
-    image: "nolan.jpeg"
+    image: "natalie.jpeg"
   },
   {
-    first_name: "Daniil",
+    first_name: "Dominic",
     last_name: "Molodkov",
     date_of_birth: "1991-04-01",
     interests: "Cats, Tacos, Secret DJing",
     allergies: "Bad tacos",
     sizes: "Tortilla (corn): 4, Tortilla (flour): 7,",
-    image: "daniil.jpeg"
+    image: "dominic.jpeg"
   },
   {
-    first_name: "Michael",
+    first_name: "Michaela",
     last_name: "Dismatsek",
     date_of_birth: "1991-10-31",
     interests: "Bagels, Guitar Hero, More Bagels",
     allergies: "Regular bread",
     sizes: "BrainStation Sweater: S",
-    image: "michael.jpeg"
+    image: "michaela.jpeg"
   },
   {
-    first_name: "Anvit",
+    first_name: "Anton",
     last_name: "Srivastav",
     date_of_birth: "1991-07-01",
     interests: "Animal Crossing, Mechanical Keyboards, Vue.JS",
     allergies: "React.JS",
     sizes: "String Instrument: XS, Animal Crossing Storage: 1600",
-    image: "anvit.jpeg"
+    image: "anton.jpeg"
   },
 ];
--- server/seed_data/users.js
+++ server/seed_data/users.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
-    email: "nolan@ribbon.io",
+    email: "natalie@ribbon.io",
     password: "password",
     uuid: "4abc150f-2532-4834-82df-fefdcc261c85",
   },
   {
-    email: "daniil@ribbon.io",
+    email: "dominic@ribbon.io",
     password: "password",
     uuid: "8d12fd03-7619-4f63-acd2-a42a26e4ad5a",
   },
   {
-    email: "michael@ribbon.io",
+    email: "michaela@ribbon.io",
     password: "password",
     uuid: "461fa967-e4e4-410f-b852-77da14c3c260",
   },
   {
-    email: "anvit@ribbon.io",
+    email: "anton@ribbon.io",
     password: "password",
     uuid: "65b01b42-0d1f-4042-a7f8-390ce1f99746",
   },
