cc0b95a8104b9d42a7725522dd39aed3d08cf3a1
Jason Florentino <jasonmflorentino@gmail.com>
2021-06-20 12:42:56 -0400

    [sever] Delete old image on image update

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -7,6 +7,7 @@ const path = require('path');
 const { v1: uuidv1 } = require('uuid');
 const fileUpload = require("express-fileupload");
 const Bookshelf = require("../bookshelf");
+const { unlink } = require('fs/promises');
 
 const router = express.Router();
 router.use(fileUpload({
@@ -37,17 +38,20 @@ router.post("/user", (req, res) => {
 
   const query = "UPDATE people SET image = :fileName WHERE (user_id = (SELECT users.id FROM users WHERE users.uuid = :uuid));"
 
-  Promise.all([
-    file.mv(path.join(__dirname, `../public/${file.name}`)),
-    Bookshelf.knex.raw(query, {fileName: file.name, uuid: userUuid}),
-  ])
-  .then(() => {
-    res.status(200).json({fileName: file.name});
-  })
-  .catch(err => {
-    res.status(500).send(err);
-    console.error("/upload file.mv ERROR:", err);
-  })
+  deleteUserImageFile(userUuid)
+    .then(() => {
+      return Promise.all([
+        file.mv(path.join(__dirname, `../public/${file.name}`)),
+        Bookshelf.knex.raw(query, {fileName: file.name, uuid: userUuid}),
+      ])
+    })
+    .then(() => {
+      res.status(200).json({fileName: file.name});
+    })
+    .catch(err => {
+      res.status(500).send(err);
+      console.error("/upload file.mv ERROR:", err);
+    })
 })
 
 /* ------------------------------------------------------------
@@ -75,18 +79,41 @@ router.post("/", (req, res) => {
   const query_details = "UPDATE gift_details SET image = :fileName WHERE (gift_id = :gift_id);"
   const query_gift = "UPDATE gifts SET date_modified = NOW() WHERE id = :gift_id;"
 
-  Promise.all([
-    file.mv(path.join(__dirname, `../public/${file.name}`)),
-    Bookshelf.knex.raw(query_details, {fileName: file.name, gift_id: gift_id}),
-    Bookshelf.knex.raw(query_gift, {gift_id: gift_id})
-  ])
-  .then(() => {
-    res.status(200).json({fileName: file.name});
-  })
-  .catch(err => {
-    res.status(500).send(err);
-    console.error("/upload file.mv ERROR:", err);
-  })
+  deleteGiftImageFile(gift_id)
+    .then(() => {
+      return Promise.all([
+        file.mv(path.join(__dirname, `../public/${file.name}`)),
+        Bookshelf.knex.raw(query_details, {fileName: file.name, gift_id: gift_id}),
+        Bookshelf.knex.raw(query_gift, {gift_id: gift_id})
+      ])
+    })
+    .then(() => {
+      res.status(200).json({fileName: file.name});
+    })
+    .catch(err => {
+      res.status(500).send(err);
+      console.error("/upload file.mv ERROR:", err);
+    })
 })
 
 module.exports = router;
+
+function deleteGiftImageFile(giftId) {
+  const queryImageName = "SELECT image FROM gift_details WHERE (gift_id = :id);"
+  return Bookshelf.knex.raw(queryImageName, {id: giftId})
+    .then(result => {
+      const filename = result[0][0].image;
+      const filepath = path.join(__dirname, `../public/${filename}`);
+      return unlink(filepath);
+    })
+}
+
+function deleteUserImageFile(userUuid) {
+  const queryImageName = "SELECT people.image FROM people WHERE people.user_id = (SELECT users.id FROM users WHERE uuid = :id);"
+  return Bookshelf.knex.raw(queryImageName, {id: userUuid})
+    .then(result => {
+      const filename = result[0][0].image;
+      const filepath = path.join(__dirname, `../public/${filename}`);
+      return unlink(filepath);
+    })
+}
